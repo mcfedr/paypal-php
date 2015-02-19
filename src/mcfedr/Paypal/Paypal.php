05b66fe01deb12548514df0416e8872b70ac1931
Fred Cox <mcfedr@gmail.com>
2015-02-19 23:20:53 +0200

    Remove incorrect license header

--- src/mcfedr/Paypal/Paypal.php
+++ src/mcfedr/Paypal/Paypal.php
@@ -1,27 +1,5 @@
 <?php
 
-/**
- * For accessing the paypal api
- * See the test file for examples of usage
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @author Fred Cox <mcfedr@gmail.com>
- * @copyright Copyright Fred Cox, 2011
- * @license http://www.gnu.org/licenses/gpl.html GNU GENERAL PUBLIC LICENSE
- */
-
 namespace Mcfedr\Paypal;
 
 use Mcfedr\Paypal\Exceptions\UnsupportedRefundException;
